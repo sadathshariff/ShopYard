b51a22d165443ac141eee422610729a9218bbce2
sadath <sadathshariff65@gmail.com>
2022-03-21 10:18:13 +0530

    feat:"made useAxios Hook"

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+export const UseAxios = (apiUrl) => {
+  const [response, setResponse] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(apiUrl);
+      setResponse(res.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, [apiUrl]);
+  return { response, loading, error };
+};
