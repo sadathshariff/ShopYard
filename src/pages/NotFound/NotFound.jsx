84bf34f88f6288850ef66847f97a7aea1b358584
sadath <sadathshariff65@gmail.com>
2022-03-30 22:45:51 +0530

    feat:"Added not Found page"

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,19 @@
+import { Button } from "../../components";
+import { useNavigate } from "react-router-dom";
 export const NotFound = () => {
-  return <div>Not Found</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="wishlist-center-div ">
+        <h1 className="headline-1">404</h1>
+        <h2 className="headline-2">Page Not Found</h2>
+        <p className="small-text-2">You ended up somewhere else</p>
+        <Button
+          name={"Shop Now"}
+          btnclass={"btn-secondary"}
+          onClick={() => navigate("/products")}
+        />
+      </div>
+    </>
+  );
 };
