7c9ea7fe22a8ab8dd897ff34bfec0f71f794680a
sadath <sadathshariff65@gmail.com>
2022-06-14 17:47:44 +0530

    Exporting

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -2,3 +2,4 @@ export { useFilter, FilterProvider } from "./filterContext/context";
 export { useAuth, AuthProvider } from "./Auth/context";
 export { useCart, CartProvider } from "./Cart/context";
 export { useWishlist, WishlistProvider } from "./Wishlist/context";
+export { useAddress, AddressProvider } from "./Address/context";
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { Cart } from "./Cart/Cart";
 export { Wishlist } from "./Wishlist/Wishlist";
 export { Login, Signup } from "./Auth/index";
 export { NotFound } from "./NotFound/NotFound";
+import { Checkout } from "./Checkout/Checkout";
